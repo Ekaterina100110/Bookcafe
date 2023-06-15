e0d689d6c95d50f368ec2d398b5e6e012074407b
Ekaterina100110 <136733552+Ekaterina100110@users.noreply.github.com>
2023-06-15 19:29:39 +0300

    Add files via upload

---
+++ файл.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$('.slider').slick({
+		arrows: true,
+		dots: false,
+		slidesToScroll: 1,
+    slidesToShow: 3,
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.sliderr').slick({
+		arrows: true,
+		dots: false,
+		slidesToScroll: 1,
+    slidesToShow: 3,
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.sliderrr').slick({
+		arrows: true,
+		dots: false,
+		slidesToScroll: 1,
+    slidesToShow: 3,
+	});
+});
+
+
+
+$(document).ready(function() {
+	$('.sliderrrr').slick({
+		arrows: true,
+		dots: false,
+		slidesToScroll: 1,
+    slidesToShow: 3,
+	});
+});
+
+
